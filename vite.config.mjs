89acf2ad126ce42ef79f267c2a26ddd966b8fccd
ZachHung <zachhung911@gmail.com>
2023-12-18 21:44:00 +0700

    feat: added husky pre-commit hook

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -8,5 +9,15 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
+    coverage: {
+      provider: 'istanbul',
+      thresholds: {
+        line: 90,
+        branches: 90,
+        functions: 90,
+        statements: 90,
+        autoUpdate: true,
+      },
+    },
   },
 });
